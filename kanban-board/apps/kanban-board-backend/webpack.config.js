25098dc42685500b803125d39ad2641d1245037e
Piotr Mieszczak <mieszczak.piotr@gmail.com>
2024-10-17 19:43:13 +0200

    rename

--- kanban-board/apps/kanban-board-backend/webpack.config.js
+++ kanban-board/apps/kanban-board-backend/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/kaban-board-backend'),
+    path: join(__dirname, '../../dist/apps/kanban-board-backend'),
   },
   plugins: [
     new NxAppWebpackPlugin({
