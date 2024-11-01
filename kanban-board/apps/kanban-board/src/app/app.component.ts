7dbb5d0e303d3531c6fa22b9210a88c888a4575a
Rafal Karpicki <rafal.karpicki@syzygy.pl>
2024-11-01 15:03:52 +0100

    Prettier config, deleted starter files, store installed

--- kanban-board/apps/kanban-board/src/app/app.component.ts
+++ kanban-board/apps/kanban-board/src/app/app.component.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Component } from "@angular/core";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'kanban-board';
 }
