6ef7fbae239664af54fd48371dd0c07756bf03e8
Piotr Mieszczak <mieszczak.piotr@gmail.com>
2024-11-10 23:04:52 +0100

    feat: add design system core styles

--- kanban-board/apps/kanban-board/src/app/app.component.ts
+++ kanban-board/apps/kanban-board/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { TuiRoot } from "@taiga-ui/core";
-import { Component } from "@angular/core";
+import { Component, HostBinding } from "@angular/core";
 
 @Component({
   standalone: true,
@@ -9,4 +9,10 @@ import { Component } from "@angular/core";
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
+  @HostBinding('class')
+  themeClass = 'theme'
+
+  // TODO theme switching
+  @HostBinding('class')
+  themeLight = 'theme-light'
 }
